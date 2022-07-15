fbeb470a23afc590619c55e7281bbb8ee09d2486
minnje97 <dlalswp112@naver.com>
2022-07-16 05:18:48 +0900

    router

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,8 +7,11 @@ function Router() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Coins />} />
-          <Route path="/:coinId/*" element={<Coin />} />
+          <Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />} />
+          <Route
+            path={`${process.env.PUBLIC_URL}/:coinId/`}
+            element={<Coin />}
+          />
         </Routes>
       </BrowserRouter>
     </>
